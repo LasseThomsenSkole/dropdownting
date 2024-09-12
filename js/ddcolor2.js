86b7acea74bf9bd567d531ff5836ca40241c24bb
LasseThomsenSkole <lath0003@stud.kea.dk>
2024-09-12 10:31:31 +0200

    ddcolor2.js now using objects for colors

--- js/ddcolor2.js
+++ js/ddcolor2.js
@@ -1,22 +1,24 @@
 let ddColor2 = document.getElementById("ddColor2");
 const pbFillDropdown2 = document.getElementById("pbFillDropdown2");
 const colArray2 = [["red","#ff5665"], ["green","#70b470"], ["yellow","#e6e675"], ["purple","#6678e6"]];
+const colObject = [{"name": "red", "id": "#ff5665"},{"name": "yellow", "id": "#e6e675"}
+    ,{"name": "green","id": "#70b470"}, {"name": "purple","id": "#6678e6"}];
 const body2 = document.querySelector('body');
 
 function fillDropdown2(item){
     const optionElement = document.createElement("option");
-    optionElement.textContent = item[0];
+    optionElement.textContent = item.name;
     console.log(optionElement.textContent);
     ddColor2.appendChild(optionElement);
 }
 
 function addColor2(){
     ddColor2.innerHTML = "";
-    colArray2.forEach(fillDropdown2);
+    colObject.forEach(fillDropdown2);
 }
 function setBackgroundColor2(){
     const selectedIndex = ddColor2.selectedIndex;
-    body2.style.backgroundColor = colArray2[selectedIndex][1]
+    body2.style.backgroundColor = colObject[selectedIndex].id
 }
 pbFillDropdown2.addEventListener('click', addColor2);
 ddColor2.addEventListener('change', setBackgroundColor2)
